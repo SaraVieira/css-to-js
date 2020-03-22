0051ae14d758e8e226df88de75d13fb679bb5c40
Sara Vieira <hey@iamsaravieira.com>
2020-03-22 19:09:15 +0100

    add tests

---
+++ src/functions/__tests__/transformCss2Obj.test.js
@@ -0,0 +1,53 @@
+import { transform } from "../transformCss2Obj";
+
+describe("transformCss2Obj", () => {
+  test("transforms a rule with a string value", () => {
+    const input = `color: red`;
+    expect(transform(input)).toBe(`{
+  color: "red",
+}`);
+  });
+
+  test("transforms a rule with a string value with single quotes", () => {
+    const input = `font-family: "Inter", sans-serif`;
+    expect(transform(input)).toBe(`{
+  fontFamily: "'Inter', sans-serif",
+}`);
+  });
+
+  test("transforms a rule with a number value", () => {
+    const input = `width: 42px`;
+    expect(transform(input)).toBe(`{
+  width: 42,
+}`);
+  });
+
+  test("transforms a rule with an arbitrary expression value", () => {
+    const input = `someProp: someExpression`;
+    expect(transform(input)).toBe(`{
+  someProp: "someExpression",
+}`);
+  });
+
+  test("transforms a simple rule", () => {
+    expect(
+      transform(`
+        display: block;
+        font-size: 16px;
+      `)
+    ).toMatchSnapshot();
+  });
+
+  test("transforms a more complex rule", () => {
+    expect(
+      transform(`
+        display: block;
+        font-size: 16px;
+        background: #1e2f5d;
+        color: #a4cff4;
+        font-family: "Inter", sans-serif;
+        font-weight: bold; 
+    `)
+    ).toMatchSnapshot();
+  });
+});
