e8dae69d1918fe24283f08954c0b02f7248a95dc
Nimo Beeren <nimo@deity.io>
2020-03-22 20:47:31 +0100

    add tests

---
+++ src/functions/__tests__/transformObj2Css.test.js
@@ -0,0 +1,50 @@
+import { transform } from "../transformObj2Css";
+
+describe("transformObj2Css", () => {
+  test("transforms a rule with a string value", () => {
+    const input = `someProp: "someValue"`;
+    expect(transform(input)).toBe(`some-prop: someValue;`);
+  });
+
+  test("transforms a rule with a string value with single quotes", () => {
+    const input = `someProp: 'someValue'`;
+    expect(transform(input)).toBe(`some-prop: someValue;`);
+  });
+
+  test("transforms a rule with a number value", () => {
+    const input = `someProp: 42`;
+    expect(transform(input)).toBe(`some-prop: 42;`);
+  });
+
+  test("transforms a rule with an arbitrary expression value", () => {
+    const input = `someProp: someExpression`;
+    expect(transform(input)).toBe(`some-prop: someExpression;`);
+  });
+
+  test("transforms a simple object", () => {
+    expect(
+      transform(`{
+        display: "block",
+        fontSize: 16
+      }`)
+    ).toMatchSnapshot();
+  });
+
+  test("transforms a more complex object", () => {
+    expect(
+      transform(`{
+      display: "block",
+      margin: { sm: 4, md: 8 },
+      padding: [2, 3],
+      background: "#1e2f5d",
+      fontSize: 16,
+      fontFamily: "'Inter', sans-serif",
+    }`)
+    ).toMatchSnapshot();
+  });
+
+  test("transforms a rule with a double quote char in its value", () => {
+    const input = `someProp: 'some"Value'`;
+    expect(transform(input)).toBe(`some-prop: some"Value;`);
+  });
+});
