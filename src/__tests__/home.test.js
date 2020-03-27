a5e49ab906e8ee6aebe42b3202547c4e562bfe0c
Nimo Beeren <nimo@deity.io>
2020-03-27 20:34:41 +0100

    more tests

--- src/__tests__/home.test.js
+++ src/__tests__/home.test.js
@@ -1,16 +1,17 @@
 import React from "react";
-import { render, wait } from "@testing-library/react";
-import transformersMock from "../transformers";
+import { render, wait, fireEvent } from "@testing-library/react";
+import mockedTransformers from "../transformers";
 import Home from "../home";
 
 // The transform functions are already unit tested, so replace them with stubs
 jest.mock("../transformers", () => {
   // TODO: use Transformer type (or will Jest KNOWWW that it should be same TYPE??? :O)
+  // mabye `typeof realTransformers`??
   return {
     css2js: {
       id: 0,
       name: "CSS => JS object",
-      transform: jest.fn(x => x),
+      transform: jest.fn(x => x.toUpperCase()),
       from: "css",
       to: "js"
     },
@@ -24,6 +25,9 @@ jest.mock("../transformers", () => {
   };
 });
 
+// This should be the same as the default transformer state in home.js
+const defaultTransformer = mockedTransformers.css2js;
+
 describe("<Home />", () => {
   test("renders without exploding", () => {
     render(<Home />);
@@ -34,15 +38,25 @@ describe("<Home />", () => {
     expect(getByRole("textbox")).toBeInTheDocument();
   });
 
-  test("calls the default transform function on load", () => {
-    render(<Home />);
-    const transformFunction = transformersMock.css2js.transform;
-    wait(() => expect(transformFunction.mock.toHaveBeenCalled(1)));
+  test("displays some output on load", async () => {
+    const { getByTitle } = render(<Home />);
+    const outputBox = getByTitle("output");
+
+    await wait(() => expect(outputBox.textContent).not.toBe(""));
   });
 
-  // TODO: do we really want to assert on the number of function calls?
-  // Why not set the input (either by firing an event or setting the value on
-  // the dom element) and wait for the output to be changed?
+  test("transforms the input when it is changed", async () => {
+    const { getByRole, getByTitle, findByText } = render(<Home />);
+    const inputBox = getByRole("textbox");
+    const outputBox = getByTitle("output");
+    const testInput = "my test input";
+    const expectedOutput = defaultTransformer.transform(testInput);
+
+    fireEvent.change(inputBox, { target: { value: testInput } });
+
+    expect(await findByText(testInput)).toBeInTheDocument();
+    await wait(() => expect(outputBox.textContent).toBe(expectedOutput));
+  });
 
   // TODO: can we somehow abstract away from the "default transformer"?
   // Maybe just use the "first" transformer as default? Though that's less explicit
