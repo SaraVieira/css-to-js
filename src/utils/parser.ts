672f64f6548ba5df7129c916040a576bed6b48ad
Nimo Beeren <nimo@deity.io>
2020-05-16 23:13:34 +0200

    simplify using parseExpression

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,28 +1,11 @@
 import * as babelParser from "@babel/parser";
-import { VariableDeclaration, ObjectExpression } from "@babel/types";
 
 export function parseJsObject(objString: string) {
-  const code = `const yourObject = ${objString}`;
+  const expression = babelParser.parseExpression(objString);
 
-  const file = babelParser.parse(code);
-
-  const declarations = file.program.body.filter(
-    node => node.type === "VariableDeclaration"
-  ) as VariableDeclaration[];
-
-  const declarators = declarations.flatMap(
-    declaration => declaration.declarations
-  );
-
-  const expressions = declarators.map(declarator => declarator.init);
-
-  const objectExpressions = expressions.filter(
-    expr => expr?.type === "ObjectExpression"
-  ) as ObjectExpression[];
-
-  if (objectExpressions.length === 0) {
-    throw new Error("No object expression found");
+  if (expression.type !== "ObjectExpression") {
+    throw new Error("Expression is not an object expression");
   }
 
-  return objectExpressions[0];
+  return expression;
 }
