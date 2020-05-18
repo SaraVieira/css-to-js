86c175bb9f666b037d8e6e3c9e7a2e84dbb0b1f9
Nimo Beeren <nimo@deity.io>
2020-05-18 20:50:34 +0200

    fix functions import

--- functions/js2jsx.js
+++ functions/js2jsx.js
@@ -1,14 +1,14 @@
-const { transform } = require("../src/transformers/js2jsx.js");
+const { transform } = require("../src/transformers/js2jsx");
 
 const headers = {
   "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "Content-Type"
+  "Access-Control-Allow-Headers": "Content-Type",
 };
 
 export async function handler({ body }) {
   return {
     headers,
     statusCode: 200,
-    body: JSON.stringify(transform(body))
+    body: JSON.stringify(transform(body)),
   };
 }
