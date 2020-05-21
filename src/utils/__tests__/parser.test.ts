e7df41e9a5fa035103238aa85a033cc06b9c7400
Nathan Thorpe <nath.thorpe@gmail.com>
2020-05-21 16:53:29 +1000

    Add css parser tests

--- src/utils/__tests__/parser.test.ts
+++ src/utils/__tests__/parser.test.ts
@@ -1,4 +1,4 @@
-import { parseJsObject } from "../parser";
+import { parseJsObject, parseCss } from "../parser";
 
 describe("parser", () => {
   describe("parseJsObject()", () => {
@@ -24,6 +24,44 @@ describe("parser", () => {
         parseJsObject("let x = 1");
       };
       expect(callWithBadParam).toThrow();
+    });
+  });
+
+  describe("parseCss()", () => {
+    test("throws when passed invalid css", () => {
+      const css = "color #fff";
+      expect(() => parseCss(css)).toThrow();
+    });
+
+    test("parses css declarations when classes, ids and elements are used", () => {
+      const css = `
+        div {
+          color: red;
+        }
+        #content {
+          width: 300px;
+        }
+        .main {
+          height: 100px;
+        }
+      `;
+      expect(parseCss(css)).toHaveLength(3);
+    });
+
+    test("returns array of PostCss AST declarations", () => {
+      const css = `
+        font-size: 10px;
+        color: #000;
+        font-weight: 700;
+        .icon {
+          width: 30px;
+        }
+      `;
+      const parsed = parseCss(css);
+      const matchDeclarations = parsed.every((item) => item.type === "decl");
+
+      expect(parsed instanceof Array).toBeTruthy();
+      expect(matchDeclarations).toBeTruthy();
     });
   });
 });
