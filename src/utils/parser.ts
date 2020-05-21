3bf38637cdbcf2f2cf40a70e5ac0f7d7434a8fc8
Nathan Thorpe <nath.thorpe@gmail.com>
2020-05-21 16:52:25 +1000

    Consolidate parsers into one file

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -1,5 +1,6 @@
 import * as babelParser from "@babel/parser";
 import { Expression, Node, ObjectExpression } from "@babel/types";
+import { ChildNode, Declaration, parse, Root } from "postcss";
 
 export function parseJsObject(input: string): [ObjectExpression, string] {
   let expression: Expression;
@@ -38,4 +39,42 @@ export function nodeToString(node: Node, code: string) {
 
   // Copy the string value directly from the input code
   return code.slice(start, end).trim();
+}
+
+/**
+ * Finds all css delcarations
+ * @param root PostCss AST Root
+ */
+function getCssProps(root: Root): Declaration[] {
+  const { nodes } = root;
+
+  if (!nodes) {
+    return [];
+  }
+
+  function iterate(nodes: ChildNode[], declarations: Declaration[] = []) {
+    nodes.forEach((item) => {
+      if (item.type === "decl") {
+        declarations.push(item);
+      } else if (item.type === "rule" && item?.nodes) {
+        iterate(item.nodes, declarations);
+      }
+    });
+
+    return declarations;
+  }
+
+  return iterate(nodes);
+}
+/**
+ * Parses css into PostCss AST and returns array of declarations
+ * @param css css string
+ */
+export function parseCss(css: string): Declaration[] {
+  try {
+    const root: Root = parse(css);
+    return getCssProps(root);
+  } catch (e) {
+    throw new Error("Could not parse CSS");
+  }
 }
