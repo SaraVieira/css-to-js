c0af95d9985197436526b5c0ed201a088b680272
Nathan Thorpe <nath.thorpe@gmail.com>
2020-05-21 16:56:52 +1000

    Convert prop and value code into functions

--- src/transformers/css2js.ts
+++ src/transformers/css2js.ts
@@ -1,34 +1,35 @@
 import * as CSS from "csstype";
-import { parseCss } from "../utils/parseCss";
+import { parseCss } from "../utils/parser";
 import { formatObject } from "../utils/formatter";
+
+// convert props
+// -webkit-flex becomes "-webkit-flex"
+// margin-bottom becomes marginBottom
+function convertCssPropToJs(prop: string): string {
+  return prop.startsWith("-")
+    ? `"${prop}"`
+    : prop.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+}
+
+function convertCssValueToJs(value: string): string {
+  return value.replace(/"/g, "'").replace(/(\d)px/g, "$1");
+}
 
 export function transform(css: string): string {
   const declarations = parseCss(css);
 
-  const cssInJs = declarations.reduce<CSS.Properties>(
-    (acc, { prop: p, value: v }) => {
-      // convert props
-      // -webkit-flex becomes "-webkit-flex"
-      // margin-bottom becomes marginBottom
-      let prop: string = p.startsWith("-")
-        ? `"${p}"`
-        : p.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
+  const cssInJs = declarations.reduce<CSS.Properties>((acc, curr) => {
+    let prop: string = convertCssPropToJs(curr.prop);
+    let value: string | number = convertCssValueToJs(curr.value);
+    const numVal = Number(value);
 
-      let value: string | number = v
-        .replace(/"/g, "'")
-        .replace(/(\d)px/g, "$1");
-
-      const numVal = Number(value);
-
-      return {
-        ...acc,
-        ...{
-          [prop]: isNaN(numVal) ? `"${value}"` : numVal,
-        },
-      };
-    },
-    {}
-  );
+    return {
+      ...acc,
+      ...{
+        [prop]: isNaN(numVal) ? `"${value}"` : numVal,
+      },
+    };
+  }, {});
 
   const objString: string = `{
     ${Object.keys(cssInJs)
