8d00b926d72200292345cae53842392ae5d745bf
Nimo Beeren <nimo@deity.io>
2020-05-24 16:38:55 +0200

    convert composed transformers to ts

--- src/transformers/css2jsx.ts
+++ src/transformers/css2jsx.ts
@@ -1,7 +1,7 @@
 import { transform as css2js } from "./css2js";
 import { transform as js2jsx } from "./js2jsx";
 
-export function transform(css) {
+export function transform(css: string): string {
   const obj = css2js(css);
   return js2jsx(obj);
 }
--- src/transformers/jsx2css.ts
+++ src/transformers/jsx2css.ts
@@ -1,7 +1,7 @@
 import { transform as jsx2js } from "./jsx2js";
 import { transform as js2css } from "./js2css";
 
-export function transform(jsx) {
+export function transform(jsx: string): string {
   const obj = jsx2js(jsx);
   return js2css(obj);
 }
