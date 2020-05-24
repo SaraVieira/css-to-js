7119c85ee16503bb3c041d09c83bd8e8950515b6
Nimo Beeren <nimo@deity.io>
2020-05-24 16:38:13 +0200

    add sanity checks for simple composed transformers

--- src/transformers/__tests__/css2jsx.test.js
+++ src/transformers/__tests__/css2jsx.test.js
@@ -1,52 +1,8 @@
 import { transform } from "../css2jsx";
 
 describe("css2jsx", () => {
-  test("transforms a rule with a string value", () => {
+  test("transforms a property", () => {
     const input = `color: red`;
-    expect(transform(input)).toBe(`color="red"`);
-  });
-
-  test("transforms a rule with a string value with single quotes", () => {
-    const input = `font-family: "Inter", sans-serif`;
-    expect(transform(input)).toBe(`fontFamily="'Inter', sans-serif"`);
-  });
-
-  test("transforms a rule with a number value", () => {
-    const input = `width: 42px`;
-    expect(transform(input)).toBe(`width={42}`);
-  });
-
-  test("transforms a rule with an arbitrary expression value", () => {
-    const input = `someProp: someExpression`;
-    expect(transform(input)).toBe(`someProp="someExpression"`);
-  });
-
-  test("transforms a simple rule", () => {
-    expect(
-      transform(`
-        display: block;
-        font-size: 16px;
-      `)
-    ).toMatchInlineSnapshot(`"display=\\"block\\" fontSize={16}"`);
-  });
-
-  test("transforms a more complex rule", () => {
-    expect(
-      transform(`
-        display: block;
-        font-size: 16px;
-        background: #1e2f5d;
-        color: #a4cff4;
-        font-family: "Inter", sans-serif;
-        font-weight: bold; 
-    `)
-    ).toMatchInlineSnapshot(`
-      "display=\\"block\\"
-      fontSize={16}
-      background=\\"#1e2f5d\\"
-      color=\\"#a4cff4\\"
-      fontFamily=\\"'Inter', sans-serif\\"
-      fontWeight=\\"bold\\""
-    `);
+    expect(transform(input)).toMatchInlineSnapshot(`"color=\\"red\\""`);
   });
 });
